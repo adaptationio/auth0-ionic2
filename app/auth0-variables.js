7668a5c6b6cfd4bb03a3570ad0d1c41426454d1e
adaptationio <willie@adaptation.io>
2016-08-13 20:38:43 +0930

    Update auth0-variables.js

--- app/auth0-variables.js
+++ app/auth0-variables.js
@@ -2,9 +2,9 @@
 var Auth0Vars = (function () {
     function Auth0Vars() {
     }
-    Auth0Vars.AUTH0_CLIENT_ID = 'Bk3Kzjrt2l64FvHbPs3vlnWoDNgicLWq';
+    Auth0Vars.AUTH0_CLIENT_ID = 'AUTH_CLIENT_ID';
     Auth0Vars.AUTH0_CALLBACK_URL = location.href;
-    Auth0Vars.AUTH0_DOMAIN = 'adaptationio.au.auth0.com';
+    Auth0Vars.AUTH0_DOMAIN = 'AUTH0';
     return Auth0Vars;
 }());
 exports.Auth0Vars = Auth0Vars;
