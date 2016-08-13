27fc1b682bd1b63494b71db91287205d86372aaf
adaptationio <willie@adaptation.io>
2016-08-13 20:33:08 +0930

    post fix

--- app/auth0-variables.js
+++ app/auth0-variables.js
@@ -2,9 +2,9 @@
 var Auth0Vars = (function () {
     function Auth0Vars() {
     }
-    Auth0Vars.AUTH0_CLIENT_ID = "7dNMEQ8RPtySGfw0A6D3oYrxXGfYo9pG";
+    Auth0Vars.AUTH0_CLIENT_ID = 'Bk3Kzjrt2l64FvHbPs3vlnWoDNgicLWq';
     Auth0Vars.AUTH0_CALLBACK_URL = location.href;
-    Auth0Vars.AUTH0_DOMAIN = "adaptationio.au.auth0.com";
+    Auth0Vars.AUTH0_DOMAIN = 'adaptationio.au.auth0.com';
     return Auth0Vars;
 }());
 exports.Auth0Vars = Auth0Vars;
