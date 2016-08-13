8abac634dc87f680ae166319c060d807c2fa8262
Paul Probinson <probinson@nextfaze.com>
2016-08-13 20:09:30 +0930

    View User Object

--- app/auth0-variables.ts
+++ app/auth0-variables.ts
@@ -1,5 +1,5 @@
 export class Auth0Vars {
-    static AUTH0_CLIENT_ID = "{CLIENT_ID}";
+    static AUTH0_CLIENT_ID = 'Bk3Kzjrt2l64FvHbPs3vlnWoDNgicLWq';
     static AUTH0_CALLBACK_URL = location.href;
-    static AUTH0_DOMAIN = "{DOMAIN}";
+    static AUTH0_DOMAIN = 'adaptationio.au.auth0.com';
 }
